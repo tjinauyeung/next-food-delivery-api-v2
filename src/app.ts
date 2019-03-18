cbb7e1f45b34d57ba7562947aa96418402866f05
Tjin Au Yeung <tjinauyeung@gmail.com>
2019-03-18 11:36:40 +0100

    setting up npm scripts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 8888;
 
+app.get("/", (req, res) => {
+  res.send({
+    foo: "Healthy"
+  });
+});
+
 app.listen(port, () => {
   console.log(`App started listening at port ${port}`);
 });
